bb3ee16c82b57b0215d50f45778a6563ca82bdd9
Sarah <sarahhtbarton@gmail.com>
2021-09-29 03:59:53 +0000

    creates homepage route

--- server.py
+++ server.py
@@ -1,12 +1,25 @@
 """Server for Shepherd Tech Challenge."""
 
-from flask import Flask
+from flask import Flask, render_template
+import requests #first have to pip3 install requests
 
 app = Flask(__name__)
 
 
-# Add routes here
+@app.route('/')
+def homepage():
+    """View homepage."""
+    
+    response = requests.get('https://gist.githubusercontent.com/mmahalwy/1459564f99b7511350d766daf3564169/raw/1bfa12209c5e91ced717007a4448e4b812304b74/forms.json')
+    forms_dict = response.json() #from json to python dictionary... well actually, list.
 
+    application_list = []
+
+    for dict in forms_dict:
+        application_list.append(dict['name'])
+
+    return render_template("homepage.html",
+                           application_list=application_list)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
