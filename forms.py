d67670b644a7c778efe8c82b1baf7d01c2fc06e9
Sarah <sarahhtbarton@gmail.com>
2021-10-01 04:11:18 +0000

    cleans up forms.py

--- forms.py
+++ forms.py
@@ -1,21 +1,17 @@
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, DecimalField, SelectField, SubmitField
-from wtforms.validators import InputRequired, NumberRange, URL
 
-app = Flask(__name__)
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, IntegerField, SelectField, SubmitField
+from wtforms.validators import InputRequired, NumberRange, URL
 
 class CompanyApplication(FlaskForm):
     company_name = StringField('What is the Company Name?', validators=[InputRequired()])
     ceo = StringField('What is the name of the CEO?', validators=[InputRequired()])
     website = StringField('Company website?', validators=[URL()])
-    # address = StringField('Address') #just a header
-        # address1 = StringField('Address', validators=[InputRequired()])
-        # address2 = StringField('Address 2')
-        # city = StringField('City')
+    address1 = StringField('Address', validators=[InputRequired()])
+    address2 = StringField('Address 2')
+    city = StringField('City')
     is_california_relevant = BooleanField('Will the contractor perform any work in California?')
-    total_compensation = DecimalField('What is the total compensation of all your workers?', validators=[InputRequired(), NumberRange(min=0)])
+    total_compensation = IntegerField('What is the total compensation of all your workers?', validators=[InputRequired(), NumberRange(min=0)])
     submit = SubmitField('Submit')
 
 class EmployeeApplication(FlaskForm):
